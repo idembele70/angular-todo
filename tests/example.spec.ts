fe79f59505cba54a9e5a09a0b7c1051d608b0e0a
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-05-12 18:08:47 +0200

    [new Todo Tests] clear input field after todo added, don't allow to add existing todo

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,36 +1,53 @@
 import { expect, test } from "@playwright/test"
 
-test.describe("add a new Todo", () => {
-  test("should add todo", async ({ page }) => {
+test.beforeEach(
+  async ({ page }) => {
     await page.goto("http://localhost:4200/")
-    const todos: string[] = [
-      "first todo", "make homework", "buy a new sneakers"
-    ]
-    // create the todo by clicking
+  }
+)
+const todos: string[] = [
+  "first todo", "make homework", "buy a new sneakers"
+]
+
+test.describe("new Todo", () => {
+  test("should add todo", async ({ page }) => {
+    // first todo
     const inputField = page.locator(".add-todo-input")
-    await inputField.fill(todos[0])
-    const didFirstTodoExist = await inputField.inputValue() === todos[0]
-    expect(didFirstTodoExist).toBeTruthy()
     const submitBtn = page.locator(".add-todo-btn")
+    const todoText = page.locator(".todo-text")
+    await inputField.fill(todos[0])
     await submitBtn.click()
-    // create the todo by pressing enter on submit Btn
+    await expect(todoText).toHaveText(todos[0])
+    // second todo
     await inputField.fill(todos[1])
-    const didSecondTodoExist = await inputField.inputValue() === todos[1]
-    expect(didSecondTodoExist).toBeTruthy()
-    await submitBtn.press("Enter")
-    // create the todo by pressing enter on submit Btn
-    await inputField.fill(todos[2])
-    const didLastTodoExist = await inputField.inputValue() === todos[2]
-    expect(didLastTodoExist).toBeTruthy()
     await inputField.press("Enter")
     // check if the todos exists
-    const isEmpty = await inputField.inputValue() === ""
-    expect(isEmpty).toBeTruthy()
-    const todoText = page.locator(".todo-text")
     await expect(todoText).toHaveText([
       todos[0],
       todos[1],
-      todos[2]
     ])
   })
+  test("should clear input after a todo is added", async ({ page }) => {
+    const inputField = page.locator(".add-todo-input")
+    await inputField.fill(todos[2])
+    await inputField.press("Enter")
+    await expect(inputField).toBeEmpty()
+  })
+  test("should not able to add existing todo", async ({ page }) => {
+    const inputField = page.locator(".add-todo-input")
+    const submitBtn = page.locator(".add-todo-btn")
+    await inputField.fill(todos[0])
+    await submitBtn.press("Enter")
+    // trying to add the same todo again
+    await inputField.fill(todos[0])
+    await submitBtn.press("Enter")
+    const inputValue = await inputField.inputValue()
+    expect(inputValue).toEqual(
+      todos[0]
+    )
+  })
+})
+
+test.describe("delete a todo", () => {
+
 })
